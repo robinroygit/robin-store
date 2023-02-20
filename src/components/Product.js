15769635570bd10c5a364c28f313a4d1d191dffa
ROBIN <apple@APPLEs-MacBook-Air.local>
2023-02-20 11:35:32 +0530

    added FormatPrice function for price of products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import FormatPrice from '../helper/FormatPrice';
 
 const Product = (curElem) => {
 
@@ -16,7 +17,7 @@ const Product = (curElem) => {
           <div className="card-data">
               <div className="card-data-flex">
                    <h3>{name}</h3>
-                   <p className="card-data--price">{price}</p>
+                   <p className="card-data--price"><FormatPrice price={price}/></p>
               </div>
 
         </div>
